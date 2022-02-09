34eb082dd02a8762c0134ec2945532d431cfd003
IlSta Lobabo Notebook <iljstase@tu-bs.de>
2022-02-09 09:58:05 +0100

    add Readme: feedforward acker

---
+++ pathcontrol/feedforward/acker/proof_acker_ff_kin.py
@@ -0,0 +1,40 @@
+from sympy import Symbol
+from sympy.printing.latex import LatexPrinter, print_latex
+from sympy.core.function import UndefinedFunction, Function
+
+
+class MyLatexPrinter(LatexPrinter):
+    """Print derivative of a function of symbols in a shorter form.
+    """
+    def _print_Derivative(self, expr):
+        function, *vars = expr.args
+        if not isinstance(type(function), UndefinedFunction) or \
+           not all(isinstance(i, Symbol) for i in vars):
+            return super()._print_Derivative(expr)
+
+        # If you want the printer to work correctly for nested
+        # expressions then use self._print() instead of str() or latex().
+        # See the example of nested modulo below in the custom printing
+        # method section.
+        return "{}_{{{}}}".format(
+            self._print(Symbol(function.func.__name__)),
+                        ''.join(self._print(i) for i in vars))
+
+
+def print_my_latex(expr):
+    """ Most of the printers define their own wrappers for print().
+    These wrappers usually take printer settings. Our printer does not have
+    any settings.
+    """
+    print(MyLatexPrinter().doprint(expr))
+
+
+y = Symbol("y")
+x = Symbol("x")
+f = Function("f")
+expr = f(x, y).diff(x, y)
+
+# Print the expression using the normal latex printer and our custom
+# printer.
+print_latex(expr)
+print_my_latex(expr)
